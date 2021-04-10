6ec406f81224c60474b7a952a89da808bcfc1b5c
Jim Chamberlin <chamberlin.jim@gmail.com>
2021-04-10 15:04:23 -0400

    Homework for Thursday

---
+++ homework_functions.py
@@ -0,0 +1,23 @@
+
+# myNumbers = [52, 50, 30, 27, 29, 29, 25, 24]
+
+# print("The smallest number is ", min(myNumbers))
+
+
+# print("The largest number is ", max(myNumbers))
+# ******************************************************
+
+
+
+myString = ["Michigan", "Mexico", "Ohio", "Indiana"]
+
+print("Here's the places I've lived :" + str(myString))
+
+res = min(myString, key = len)
+
+print("The smallest string in the places I've lived is: " + res)
+
+res = max(myString, key = len)
+
+print("The Largest string in the places I've lived is: " + res)
+# ***************************************************************
